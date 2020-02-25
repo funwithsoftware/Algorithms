c5220e7d034d4df94c62f52dc7bb32d855e28017
funwithsoftware <arumugamgandhi@gmail.com>
2020-02-25 19:30:01 +0530

    Included function printrepeatedchar()

--- mystring.h
+++ mystring.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 class MyString
 {
@@ -13,6 +14,34 @@ MyString(string S);
 
 // Return the first occurrence of non repeating character in the given string
 int find_first_occurrence_of_non_repeating_char(string s);
+  
+// Print the repeated characters in the given string
+void printrepeatedchar(string s)
+{
+  map<char, int> m;
+  for(int i=0;i<s.length();i++)
+  {
+      if(s[i] != ' ')
+      {
+          std::map<char, int>::iterator itr = m.find(s[i]);
+          if(itr!=m.end())
+          {
+              m[s[i]]++;
+          }
+          else
+          {
+              m.insert ( pair <char, int> ( s[i], 1) );
+          }
+      }
+  }
+  std::map<char, int>::iterator itr = m.begin();
+  for(;itr!=m.end();itr++){
+     if( (*itr).second  > 1)
+     {
+         cout << (*itr).first << endl;
+     }
+  }
+}
 
 private:
 string s;
